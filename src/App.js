424aa13ece062e585748cfa0cd18a2321bbf4b7d
oamk-isri <n2isri00@students.oamk.fi>
2023-02-27 22:47:24 +0200

    implement a cat fetcher

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {Cats()}
     </div>
   );
+}
+
+const catURL = "https://aws.random.cat/meow"
+
+function Cats() {
+  const [cat, setCat] = useState("");
+
+  async function handleCat(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch(catURL);
+
+      if (response.ok) {
+        const json = await response.json();
+        setCat(json.file)
+      }
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h2>Fetching Cats</h2>
+      <form onSubmit={handleCat}>
+        <button>Get a cat</button>
+      </form>
+      <div class="cat-container">
+      <img src={cat}></img>
+      </div>
+    </div>
+  ); 
 }
 
 export default App;
